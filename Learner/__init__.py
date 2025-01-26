88920fef88aed1264e59f6aec5110901cf45337d
yangymargaret <yy3@andrew.cmu.edu>
2025-01-25 23:09:08 -0500

    Create __init__.py

---
+++ Learner/__init__.py
@@ -0,0 +1,11 @@
+"""
+Learner
+
+python package for transcription factor binding prediction
+"""
+from . import test_learner_compute_1
+from . import test_learner_compute_2
+from . import test_learner_compute_3
+
+__version__ = "0.1.0"
+__author__ = "Yang"
